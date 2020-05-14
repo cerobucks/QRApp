2dd09da241d1921b38a9bb10d90d7c1cdbe417b7
user172869 <user172869@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-05-14 14:35:30 -0400

    Fisrt build of app in ios

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  final GlobalKey qrKey = GlobalKey(debugLabel: "QR");
+  var qrText = "";
+  QRViewController controller;
+
+  @override
+  void initState() {
+      super.initState();
+     
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: Text(widget.title),
+      ),
+      body: Column(children: <Widget>[
+        Expanded(
+            flex: 5,
+            child:  QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            )
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text('Scan result: $qrText'),
+            ),
+          )
+      ],),
+    );
+  }
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+  
+      controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData;
+      });
+    });
+  }
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
